9e0d744ddb17e366ae633bc214a4576799e131d6
zhengxu-eng <56611052+zhengxu-eng@users.noreply.github.com>
2020-02-14 06:34:17 +0800

    Update EE551_HW1.py

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -16,11 +16,11 @@ def numbers_and_strings():
     # Assign a string "Stevens" to a variable y
     y = "Stevens"
     # Repeat variable y 5 times
-
+    print(y * 5)
     # What is the length of z?
-    len(z)
+    print(len(z))
     # Concatenate variable y with string " is good"
-    y + "is good"
+    print(y + "is good")
     # Replace "good" with "awesome" in variable m and assign it to a new variable n
     n = m.replace("good", "awesome")
     return x, y, z, length, m, n
@@ -43,9 +43,14 @@ def lists():
     list = [[1, 4, 5], [6, 10, 11], [12, 17, 38]]
 
     # Collect the items in the last column of matrix A using list comprehension
-    list[2]
+    print[row[-1] for row in list]
     # Collect only the even items of the diagonal of matrix A using list comprehension
-
+    res = []
+    for row in list:
+        for x in row:
+            if x % 2 == 0:
+                res.append(x)
+    print(res)          
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
@@ -61,25 +66,29 @@ def dictionaries():
     #   fruit => "apple"
     #   quantity => 4
     #   color => "green"
-    dic = 
+    f = {"fruit" : "apple", "quantity" : 4, "color" : "green"}
 
     # Get the item in dictionary f that the key "fruit" maps to
-
+    f['fruit']
     # Increase the quantity of f by 1
     # IMPLEMENT IT HERE
+    f['quantity'] += 1
 
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
+    dic = {'name' : {'first_name': 'Grace', 'last_name': 'Hopper'}, 
+           'jobs': ['scientist': 'engineer'], 
+           'age': 85}
 
     # Add "programmer" to the list of jobs Grace has
     # IMPLEMENT IT HERE
-
+    dic['jobs'].append('programmer')
     # Get the third job Grace has that you recently added
-
+    print(dic['jobs'][2])
     # Use the sort() function to get sorted keys of amazing_grace in alphabetically ascending order
-
+    print(dic['name'].keys().sort())aa
     return a, f, p, k
 
 numbers_and_strings()
